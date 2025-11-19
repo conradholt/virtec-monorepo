72baf299c763885580656cb06034f4b5f5fb1638
Erik <ewpeters@asu.edu>
2025-11-19 15:44:07 -0700

    add wasi

--- apps/wasm/index.js
+++ apps/wasm/index.js
@@ -1,9 +1,21 @@
 // index.js
 const fs = require('fs').promises;
+const { argv, env } = require('process');
+const { WASI } = require('wasi');
 
 async function main() {
-  const bytes = await fs.readFile(new URL('./hello.wasm', import.meta.url)); // works in Node >=14 with --experimental-modules or with CommonJS use __dirname
-  const { instance } = await WebAssembly.instantiate(bytes, {});
+  const wasi = new WASI({
+//   version: 'preview1',
+//   args: argv,
+//   env,
+//   preopens: {
+//     '/sandbox': '/path/on/host',
+//   },
+  });
+  const bytes = await fs.readFile(new URL('./hello.wasm', import.meta.url));
+  // works in Node >=14 with --experimental-modules or with CommonJS use __dirname
+
+  const { instance } = await WebAssembly.instantiate(bytes, wasi.getImportObject());
   const add = instance.exports.add;
   console.log("3 + 4 =", add(3,4));
 }
